08fc708dc6daca1340ccd36540bd84b87e86c5a1
SyedMyC <132682676+Syed-MyC@users.noreply.github.com>
2023-05-18 15:47:55 +0530

    Add files via upload

---
+++ MyCaptain Assignment 1.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+#Data Structure
+import math as M
+
+
+Radius = float (input ("Input the radius of the circle: "))  
+
+
+areaofthecircle = M.pi* Radius * Radius  
+
+
+print (" The area of the circle with radius: ", areaofthecircle)  
+ 
